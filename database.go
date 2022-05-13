ddca58a82a4e17c1cff5350af9952d9ea52b0c7b
Dhruv <dhruvahuja2k@gmail.com>
2022-05-14 01:21:24 +0530

    feat: add database.go file
    - make func to connect to sqlite db

---
+++ database.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func connectToDB(dbPath string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbPath)
+	return db, err
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/dhruv-ahuja/go-api
 
 go 1.18
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/mattn/go-sqlite3 v1.14.13
+)
