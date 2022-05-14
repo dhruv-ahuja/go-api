22509727a1be79bab313a0e22650e21714df8b72
Dhruv <dhruvahuja2k@gmail.com>
2022-05-14 13:12:49 +0530

    feat: add 'Book' struct as DB model

--- database.go
+++ database.go
@@ -11,6 +11,18 @@ func connectToDB(dbPath string) (*sql.DB, error) {
 	return db, err
 }
 
+type Book struct {
+	// all field tags must be exported through capitalization
+	// if they are to be used in the JSON encodings
+	ID int `json:"id"`
+	// `omitempty` excludes the field from the JSON encoding if its empty
+	ISBN   int    `json:"isbn,omitempty"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Genre  string `json:"genre"`
+	Year   int    `json:"year,omitempty"`
+}
+
 func createTable(db *sql.DB) error {
 	query := `CREATE TABLE IF NOT EXISTS books (
 		id INTEGER PRIMARY KEY,
