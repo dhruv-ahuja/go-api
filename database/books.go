f54478589faf0a5356d8f3fb83c412aa6fbbe5a8
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2022-05-26 12:30:38 +0530

    feat: add a new struct BookShelf in books.go
    - BookShelf implements the BookStore interface
    
    - remove *sql.DB from all methods in the BookStore interface

--- database/books.go
+++ database/books.go
@@ -9,13 +9,14 @@ import (
 )
 
 type BookStore interface {
-	GetBook(*sql.DB, int) (*Book, error)
-	GetBooks(*sql.DB) ([]Book, error)
-	AddBook(*sql.DB, *json.Decoder) (*Book, error)
-	UpdateBook(*sql.DB, *json.Decoder, int) (*Book, error)
-	DeleteBook(*sql.DB, int) (sql.Result, error)
+	GetBook(int) (*Book, error)
+	GetBooks() ([]Book, error)
+	AddBook(*json.Decoder) (*Book, error)
+	UpdateBook(*json.Decoder, int) (*Book, error)
+	DeleteBook(int) (sql.Result, error)
 }
 
+// Book contains the properties that the books stored in the DB have.
 type Book struct {
 	// all field tags must be exported through capitalization
 	// if they are to be used in the JSON encodings
@@ -28,12 +29,20 @@ type Book struct {
 	Year   int    `json:"year,omitempty"`
 }
 
+// BookShelf is the struct that implements BookStore here.
+// The 'Book' struct is used to store data when performing operations on the
+// BookShelf. So, a BookShelf stores many books and acts as a small-scale
+// representation of what a BookStore would be.
+type BookShelf struct {
+	DB *sql.DB
+}
+
 // GetBook returns a book from the database given its ID
-func GetBook(db *sql.DB, bookID int) (*Book, error) {
+func (b *BookShelf) GetBook(bookID int) (*Book, error) {
 	query := `SELECT * FROM books WHERE id=?;`
 	book := &Book{}
 
-	row := db.QueryRow(query, bookID)
+	row := b.DB.QueryRow(query, bookID)
 	// we always need to be using pointers with Scan, doesn't matter if the
 	// destination struct has been intiliazed with its pointer or not
 	if err := row.Scan(&book.ID, &book.ISBN, &book.Title, &book.Author,
@@ -45,11 +54,11 @@ func GetBook(db *sql.DB, bookID int) (*Book, error) {
 }
 
 // GetBooks returns all books from the database
-func GetBooks(db *sql.DB) ([]Book, error) {
+func (b *BookShelf) GetBooks() ([]Book, error) {
 	query := "SELECT * FROM books;"
 	books := []Book{}
 
-	rows, err := db.Query(query)
+	rows, err := b.DB.Query(query)
 	if err != nil {
 		return nil, err
 	}
@@ -79,7 +88,7 @@ func GetBooks(db *sql.DB) ([]Book, error) {
 }
 
 // AddBook receives the book in as POST body and adds it to the database
-func AddBook(db *sql.DB, decoder *json.Decoder) (*Book, error) {
+func (b *BookShelf) AddBook(decoder *json.Decoder) (*Book, error) {
 	book := &Book{}
 
 	// this will return an error if there is a mismatch between the data received
@@ -92,7 +101,7 @@ func AddBook(db *sql.DB, decoder *json.Decoder) (*Book, error) {
 	query := `INSERT INTO books (isbn, title, author, genres, year) VALUES
 (?, ?, ?, ?, ?) RETURNING *;`
 
-	row := db.QueryRow(query, book.ISBN, book.Title, book.Author,
+	row := b.DB.QueryRow(query, book.ISBN, book.Title, book.Author,
 		book.Genres, book.Year)
 
 	// res will store the result returned by the DB, to be sent back to the user
@@ -109,7 +118,7 @@ func AddBook(db *sql.DB, decoder *json.Decoder) (*Book, error) {
 
 // UpdateBook receives the book to be updated as POST body and updates it in
 // the database.
-func UpdateBook(db *sql.DB, decoder *json.Decoder, bookID int) (*Book, error) {
+func (b *BookShelf) UpdateBook(decoder *json.Decoder, bookID int) (*Book, error) {
 	book := &Book{}
 
 	decoder.DisallowUnknownFields()
@@ -121,7 +130,7 @@ func UpdateBook(db *sql.DB, decoder *json.Decoder, bookID int) (*Book, error) {
 	WHERE id=? RETURNING *;`
 
 	// bookID is entered separately since its being received through the URL
-	row := db.QueryRow(query, book.ISBN, book.Title, book.Author,
+	row := b.DB.QueryRow(query, book.ISBN, book.Title, book.Author,
 		book.Genres, book.Year, bookID)
 
 	res := &Book{}
@@ -135,10 +144,10 @@ func UpdateBook(db *sql.DB, decoder *json.Decoder, bookID int) (*Book, error) {
 
 // DeleteBook receives the book to deleted as POST body and remvoes it from
 // the database.
-func DeleteBook(db *sql.DB, bookID int) (sql.Result, error) {
+func (b *BookShelf) DeleteBook(bookID int) (sql.Result, error) {
 	query := `DELETE FROM books WHERE id=?;`
 
-	res, err := db.Exec(query, bookID)
+	res, err := b.DB.Exec(query, bookID)
 
 	if err != nil {
 		return nil, err
